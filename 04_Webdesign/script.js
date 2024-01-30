e6bd5797fb9ab70a9c03a14fef071d743ca3cb73
HastagKc <chaudharykshittiz950@gmail.com>
2024-01-30 17:31:58 +0545

    css complete

--- 04_Webdesign/script.js
+++ 04_Webdesign/script.js
@@ -1,27 +1,14 @@
-/*===== MENU SHOW Y HIDDEN =====*/
-const navMenu = document.getElementById("nav-menu"),
-  toggleMenu = document.getElementById("nav-toggle"),
-  closeMenu = document.getElementById("nav-close");
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
 
-/*SHOW*/
-toggleMenu.addEventListener("click", () => {
-  navMenu.classList.toggle("show");
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
 });
 
-/*HIDDEN*/
-closeMenu.addEventListener("click", () => {
-  navMenu.classList.remove("show");
-});
-
-/*===== ACTIVE AND REMOVE MENU =====*/
-const navLink = document.querySelectorAll(".nav__link");
-
-function linkAction() {
-  /*Active link*/
-  navLink.forEach((n) => n.classList.remove("active"));
-  this.classList.add("active");
-
-  /*Remove menu mobile*/
-  navMenu.classList.remove("show");
-}
-navLink.forEach((n) => n.addEventListener("click", linkAction));
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
